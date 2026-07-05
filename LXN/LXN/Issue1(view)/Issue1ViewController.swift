4af67d89986eae3493d1f133b4174ab642b42f67
Nguyễn Công Tuấn <147248673+TOIsMeMan@users.noreply.github.com>
2026-07-05 21:59:59 +0700

    init

---
+++ LXN/LXN/Issue1(view)/Issue1ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  Issue1ViewController.swift
+//  LXN
+//
+//  Created by LXN on 5/7/26.
+//
+
+import UIKit
+
+final class Issue1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+}
+
+// MARK: - Setup
+extension Issue1ViewController {
+    private func setupUI() {
+        title = "Issue 1"
+    }
+}
